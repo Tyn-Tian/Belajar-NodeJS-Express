77ea94fee3faf963eae9f177c6fb7efd541068d2
Christian <kristian2015ok@gmail.com>
2024-06-07 18:39:54 +0700

    Add: Basic Routing

--- src/hello.js
+++ src/hello.js
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Helo World");
+});
+
+app.get('/tian', (req, res) => {
+    res.send("Hello Tian");
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
