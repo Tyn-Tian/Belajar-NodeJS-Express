c645b9297f32532a617e6b6d0651c46f6e5a15a5
Christian <kristian2015ok@gmail.com>
2024-06-08 12:06:57 +0700

    Add: Static File

---
+++ test/static-file.test.js
@@ -0,0 +1,29 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+
+app.use('/static', express.static(__dirname + "/static"));
+
+app.get('/', (req, res) => {
+    res.send(`Hello Response`);
+});
+
+app.get('/contoh.text', (req, res) => {
+    res.send(`Hello Response`);
+});
+
+test('Test Static File', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe("Hello Response");
+});
+
+test('Test Static File /contoh.txt', async () => {
+    const response = await request(app).get('/contoh.text');
+    expect(response.text).toContain("Hello Response");
+});
+
+test('Test Static File /static/contoh.txt', async () => {
+    const response = await request(app).get('/static/contoh.txt');
+    expect(response.text).toContain('This is sample text');
+});
